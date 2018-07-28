1f55e0af2ffd8fafb4e9b8761f642f15314f7457
szymslow12 <szym.slow12@gmail.com>
2018-07-28 16:10:35 +0200

    add static createFromObject

---
+++ Note.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import {NoteContent} from "./NoteContent.js";
+
+export class Note {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    createHTMLNote() {
+        let note = document.createElement("div");
+        note.setAttribute("class", "note");
+        let noteContent = new NoteContent(this.title, this.content);
+        let title = noteContent.getTitle();
+        let content = noteContent.getNoteContent();
+        note.appendChild(title);
+        note.appendChild(content);
+        return note;
+    }
+
+    static createFromObject(object) {
+        let note = new Note(object.title, object.content);
+        return note;
+    }
+}
